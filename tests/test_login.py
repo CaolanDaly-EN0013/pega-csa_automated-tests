b12db7689b7e8e6a8538e93046f597218236e80a
CaolanDaly <caolan.daly@aflacni.com>
2020-06-25 09:14:10 +0100

    Add test_login.py to verify users can login with valid credentials

---
+++ tests/test_login.py
@@ -0,0 +1,16 @@
+'''
+These tests cover logging in to the Pega Sandbox environment.
+'''
+
+def test_login():
+
+    # Given the Pega Sandbox login page is displayed
+    # TODO
+
+    # When the user enters a valid username and password
+    # TODO
+
+    # Then the user will be navigated to the case worker home page
+    # TODO
+
+    raise Exception("Incomplete test")
