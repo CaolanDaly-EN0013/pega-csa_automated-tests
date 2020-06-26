8fbe66fbfd859db97224b677d3896150411c9d35
CaolanDaly <caolan.daly@aflacni.com>
2020-06-26 09:08:21 +0100

    Updated test_login.py

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,11 +2,18 @@
 These tests cover logging in to the Pega Sandbox environment.
 '''
 
+from pages.login import PegaLoginPage
+from pages.home import PegaHomePage
 
 def test_login(browser):
 
+    login_page = PegaLoginPage(browser)
+    home_page = PegaHomePage(browser)
+    USERNAME = "author@gogoroad"
+    PASSWORD = "pega123!"
+
     # Given the Pega Sandbox login page is displayed
-    # TODO
+    login_page.load()
 
     # When the user enters a valid username and password
     # TODO
