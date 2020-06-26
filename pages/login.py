0c49f4591869ed35ae70efef1909fd19e8742997
CaolanDaly <caolan.daly@aflacni.com>
2020-06-26 09:03:12 +0100

    Added login.py page object

---
+++ pages/login.py
@@ -0,0 +1,17 @@
+'''
+This module contains PegaLoginPage,
+the page object for Pega Academy demo system login page.
+'''
+
+class PegaLoginPage:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def load(self):
+        # TODO
+        pass
+
+    def enter_credentials(self, username, password):
+        # TODO
+        pass
