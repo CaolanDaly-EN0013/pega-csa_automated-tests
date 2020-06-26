dde4341a5394c30e264166936ebf8f1b4958f8ba
CaolanDaly <caolan.daly@aflacni.com>
2020-06-26 09:05:06 +0100

    Added home.py page object

---
+++ pages/home.py
@@ -0,0 +1,13 @@
+'''
+This module contains PegaHomePage,
+the page object for Pega Academy demo system home page.
+'''
+
+class PegaHomePage:
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    def title(self):
+        # TODO
+        return ''
